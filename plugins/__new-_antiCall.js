73cd37fe6e3453ec2e958f81d62a0bbcac2b74a6
hackerkhan1212 <94687058+hackerkhan1212@users.noreply.github.com>
2022-11-19 21:00:12 +0500

    Update __new-_antiCall.js

--- plugins/__new-_antiCall.js
+++ plugins/__new-_antiCall.js
@@ -14,7 +14,7 @@ export async function all(m) {
 	if(!setting.anticall) return 
 	
 	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
-		await this.reply(m.chat, 'kamu Di blockir oleh bot karena telah melanggar aturan bot\n\n*📮Dilarang menelepon Bot!*', null)
+		await this.reply(m.chat, 'you Blocked by bots for violating bot rules\n\n*📮Dont Call Bots!*', null)
 		await delay(1000)
 		await this.updateBlockStatus(m.chat, "block")
 	}
