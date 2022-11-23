b1e32ad0d2f2da9660ff7461de5d5b962768cb0d
hackerkhan1212 <94687058+hackerkhan1212@users.noreply.github.com>
2022-11-23 21:02:09 +0500

    Create prem-animethighss.js

---
+++ plugins/prem-animethighss.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+let handler = async (m, {command, conn}) => {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/animethighss?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+handler.help = ['animethighss']
+handler.tags = ['nsfw', 'premium']
+handler.command = /^(meme)$/i
+export default handler
