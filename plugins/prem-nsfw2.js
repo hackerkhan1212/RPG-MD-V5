dde269318f5e64b21f2801fe1f1efa203cf7504f
hackerkhan1212 <94687058+hackerkhan1212@users.noreply.github.com>
2022-11-24 20:28:37 +0500

    Create prem-nsfw2.js

---
+++ plugins/prem-nsfw2.js
@@ -0,0 +1,346 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, {m, {command, conn}) => {
+
+  if (command == 'bj') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/biganimetiddies?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'blowjob') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/blowjob?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'classicnsfw') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/classic?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'cum') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/cum?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'cum_jpg') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/cum_jpg?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'ecchi') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/ecchi?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'ero') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/ero?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'erokemo') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/erokemo?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'eroyuri') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/eroyuri?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'erofeet') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/erofeet?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'erok') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/erok?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'eron') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/eron?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'feetg') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/feetg?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'femdom') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/femdom?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'fox_girl') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/fox_girl?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'futanari') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/futanari?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'hentai2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/hentai?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'hentai3') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/hentai?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'hentaifemdom') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/hentaifemdom?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'hentai4everyone') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/hentai4everyone?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'hololewd') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/hololewd?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'holoero') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/holoero?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'hololewd2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/hololewd?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'keta') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/keta?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'kuni') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/kuni?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'lewd') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/lewd?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'lewdanimegirls') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/lewdanimegirls?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'hololewd2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/hololewd?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'lewdk') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/lewdk?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'lewdkemo') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/lewdkemo?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'loli3') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/loli?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'milf2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/milf?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'nsfw_avatar') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/nsfw_avatar?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'blowjob2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/blowjob?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'loli4') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'chiisaihentai') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/chiisaihentai?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'neko2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/neko?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'nsfw_neko_gif') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/nsfw_neko_gif?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'trap') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/trap?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'waifu3') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/waifu?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'pussy2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/pussy?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'pussy_jpg') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/pussy_jpg?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'random_hentai_gif') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/random_hentai_gif?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'sideoppai') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/sideoppai?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'solo') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/solo?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'solog') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/solog?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'tits2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/tits?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'trap2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/trap?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+if (command == 'yaoi2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random/nsfw/yaoi?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+if (command == 'yuri2') {
+let res = await axios(`https://api.lolhuman.xyz/api/random2/yuri?apikey=${global.lolkeysapi}`)
+let json = res.data
+let url = json.url
+conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', `/${command}`]], m)
+}
+
+
+
+
+}
+handler.help = ['bj', 'blowjob', 'classicnsfw', 'cum', 'cum_jpg', 'ecchi', 'ero', 'erokemo', 'eroyuri', 'erofeet', 'erok', 'eron', 'feetg', 'femdom', 'fox_girl', 'futanari', 'hentai2', 'hentai3', 'hentaifemdom', 'hentai4everyone', 'hololewd', 'holoero', 'hololewd2vketa kuni', 'lewd', 'lewdanimegirls', 'lewdk', 'lewdkemo', 'loli3', 'milf2', 'nsfw_avatar', 'blowjob2', 'loli4', 'chiisaihentai', 'neko2', 'nsfw_neko_gif', 'trap', 'waifu3', 'pussy2', 'pussy_jpg', 'random_hentai_gif', 'sideoppai', 'solo', 'solog', 'tits2', 'trap2', 'yaoi2', 'yuri2'].map(v => v + ' <>')
+handler.tags = ['nsfw', 'premium']
+handler.command = ['bj', 'blowjob', 'classicnsfw', 'cum', 'cum_jpg', 'ecchi', 'ero', 'erokemo', 'eroyuri', 'erofeet', 'erok', 'eron', 'feetg', 'femdom', 'fox_girl', 'futanari', 'hentai2', 'hentai3', 'hentaifemdom', 'hentai4everyone', 'hololewd', 'holoero', 'hololewd2vketa kuni', 'lewd', 'lewdanimegirls', 'lewdk', 'lewdkemo', 'loli3', 'milf2', 'nsfw_avatar', 'blowjob2', 'loli4', 'chiisaihentai', 'neko2', 'nsfw_neko_gif', 'trap', 'waifu3', 'pussy2', 'pussy_jpg', 'random_hentai_gif', 'sideoppai', 'solo', 'solog', 'tits2', 'trap2', 'yaoi2', 'yuri2']
+
+export default handler
