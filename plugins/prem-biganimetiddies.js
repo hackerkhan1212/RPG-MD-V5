e534a1d65f23cf9732c50796cd98955745191b8d
hackerkhan1212 <94687058+hackerkhan1212@users.noreply.github.com>
2022-11-24 18:32:23 +0500

    Update prem-biganimetiddies.js

--- plugins/prem-biganimetiddies.js
+++ plugins/prem-biganimetiddies.js
@@ -7,5 +7,5 @@ conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', 
 }
 handler.help = ['biganimetiddies']
 handler.tags = ['nsfw', 'premium']
-handler.command = /^(meme)$/i
+handler.command = /^(biganimetiddies)$/i
 export default handler
