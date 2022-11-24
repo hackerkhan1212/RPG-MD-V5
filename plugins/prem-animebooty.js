ee0ec68faa0505c50fe08d2bfa1d8216cf1222fe
hackerkhan1212 <94687058+hackerkhan1212@users.noreply.github.com>
2022-11-24 18:31:32 +0500

    Update prem-animebooty.js

--- plugins/prem-animebooty.js
+++ plugins/prem-animebooty.js
@@ -7,5 +7,5 @@ conn.sendButton(m.chat, `*${command}*`.trim(), author, url, [['🔄 NEXT 🔄', 
 }
 handler.help = ['animebooty']
 handler.tags = ['nsfw', 'premium']
-handler.command = /^(meme)$/i
+handler.command = /^(animebooty)$/i
 export default handler
