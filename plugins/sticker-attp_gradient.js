597ee29b49eea14911a83ef06c9644f6de8595e1
hackerkhan1212 <94687058+hackerkhan1212@users.noreply.github.com>
2022-11-24 20:37:36 +0500

    Update sticker-attp_gradient.js

--- plugins/sticker-attp_gradient.js
+++ plugins/sticker-attp_gradient.js
@@ -7,7 +7,7 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
 let name = await conn.getName(who)
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-let urls = ['https://violetics.pw/api/canvas/quotes?apikey=beta&text=' + teks + '&author=' + name,
+let urls = ['https://violetics.pw/api/canvas/quotes?apikey=${global.violetics}&text=' + teks + '&author=' + name,
 'https://api.lolhuman.xyz/api/attp2?apikey=' + global.lolkey + '&text=' + teks,
 'https://api.lolhuman.xyz/api/attp?apikey=' + global.lolkey + '&text=' + teks,
 'https://api.lolhuman.xyz/api/hartacustom?apikey=' + global.lolkey + '&text=' + teks,
@@ -19,9 +19,9 @@ let urls = ['https://violetics.pw/api/canvas/quotes?apikey=beta&text=' + teks + 
 'https://api.lolhuman.xyz/api/ttp?apikey=' + global.lolkey + '&text=' + teks,
 'https://leyscoders-api.herokuapp.com/api/ttp1?text=' + teks + '&apikey=MIMINGANZ',
 'https://leyscoders-api.herokuapp.com/api/ttp2?text=' + teks + '&apikey=MIMINGANZ',
-'https://violetics.pw/api/canvas/attp-gradient2?apikey=beta&text=' + teks,
-'https://violetics.pw/api/canvas/attp-gradient?apikey=beta&text=' + teks,
-'https://violetics.pw/api/canvas/ttp-gradient?apikey=beta&text=' + teks]
+'https://violetics.pw/api/canvas/attp-gradient2?apikey=${global.violetics}&text=' + teks,
+'https://violetics.pw/api/canvas/attp-gradient?apikey=${global.violetics}&text=' + teks,
+'https://violetics.pw/api/canvas/ttp-gradient?apikey=${global.violetics}&text=' + teks]
 
             let lisn = ["Quotes",
             "Attp 2",
@@ -45,8 +45,8 @@ let urls = ['https://violetics.pw/api/canvas/quotes?apikey=beta&text=' + teks + 
 		rowId: usedPrefix + 'get ' + urls[v]
 	}))
 	let button = {
-		buttonText: `☂️ Tema Disini ☂️`,
-		description: `⚡ Silakan pilih tema di tombol di bawah...\n*Teks yang anda kirim:* ${text}\n\nKetik ulang *${usedPrefix + command}* teks anda untuk mengubah teks lagi`,
+		buttonText: `☂️ Theme Here ☂️`,
+description: `⚡ Please choose a theme in the button below...\n*Text to send:* ${text}\n\nRetype *${usedPrefix + command}* your text to change the text again`,
 		footerText: wm
 	}
 	return await conn.sendListM(m.chat, button, row, m)
